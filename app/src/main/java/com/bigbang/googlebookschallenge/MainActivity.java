5372d80f512e5ae15ebee66f2c7e4c66b48be354
Jean-Paul Deshaies <jdeshaies1982@gmail.com>
2020-03-24 16:08:59 -0400

    base file - model - util packages

---
+++ app/src/main/java/com/bigbang/googlebookschallenge/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bigbang.googlebookschallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/bigbang/googlebookschallenge/util/Constants.java
@@ -0,0 +1,14 @@
+package com.bigbang.googlebookschallenge.util;
+
+public class Constants {
+
+    //Error Messages
+    static final String TAG = "TAG_X";
+    static final String ERROR_PREFIX = "Error: ";
+    public static final String RESULTS_NULL = "Results were null";
+
+    //Retrofit call
+    public static final String GET_URL_POSTFIX = "/users/{user_name}/repos";
+    public static final String USER_NAME = "user_name";
+    public static final String BASE_URL = "https://api.github.com/";
+}
